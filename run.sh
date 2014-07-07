86c28a9a0124666947f8733268aaa8745751436a
William Hanwoody <hanwoody@gmail.com>
2014-07-07 11:45:49 +0800

    haproxy use run.sh to run

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HAPROXY=/usr/local/haproxy/sbin/haproxy
+CONF=/etc/haproxy/haproxy.cfg
+PID=/var/run/haproxy.pid
+
+grep -E "stats auth" $CONF | grep -q '{random}' &&
+PASS=$(echo `for i in {1..5}; do echo $RANDOM; done`|md5sum|cut -c1-10) &&
+sed -i "s/{random}/$PASS/" $CONF &&
+echo "Your haproxy stats password: $PASS"
+
+exec $HAPROXY -f $CONF -p $PID
