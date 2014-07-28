970eb9b29df0e9bb1101948ff5d6f983702951f4
William Hanwoody <hanwoody@gmail.com>
2014-07-28 17:57:41 +0800

    wait 7s

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -15,11 +15,12 @@ sid=`get_sid`
 if docker top $sid > /dev/null; then
   /usr/local/bin/nicedocker exec $sid sh -c "$HAPROXY -f $CONF -sf \$(ps ax|grep haprox[y]|cut -c1-5)"
   count=0
-  while [ $count -lt 500 ]; do
+  while [ $count -lt 15 ]; do
     docker ps | grep -q $sid || break
     sleep 0.5
     count=$((count+1))
   done
+  docker top $sid > /dev/null 2>/dev/null && docker kill $sid
 else
   echo service $sid no run.
 fi
